bc4a0313e83470c45724df0d56da5f5307fe822e
xiangyuc <xiangyu.chang@hotmail.com>
2017-07-31 22:09:39 +0800

    add flask migrate

--- lessonboard/__init__.py
+++ lessonboard/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from lessonboard.extensions import db, bcrypt
+from lessonboard.extensions import db, bcrypt, migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db.init_app(app)
 bcrypt.init_app(app)
+migrate.init_app(app, db)
 
 
 import lessonboard.views
--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 from flask_script import Manager
+from flask_migrate import MigrateCommand
 from lessonboard import app
-from lessonboard.models import db
+from lessonboard.extensions import db, migrate
 
 
 manager = Manager(app)
+
+
+migrate.init_app(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
