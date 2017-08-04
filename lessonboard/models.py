8365e62c8e3dd003f5f50e53921d534dc97e01e5
xiangyuc <xiangyu.chang@hotmail.com>
2017-08-04 13:34:48 +0800

    fix flask migrate

--- lessonboard/models.py
+++ lessonboard/models.py
@@ -48,6 +48,7 @@ class Board(db.Model):
     time_created = db.Column(db.DateTime)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    chapters = db.relationship('Chapter', backref='board', lazy='dynamic')
 
     def __init__(self, title, author_id, subject_id):
         self.title = title
@@ -57,3 +58,42 @@ class Board(db.Model):
 
     def __repr__(self):
         return '<Board {}>'.format(self.title)
+
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    time_created = db.Column(db.DateTime)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+    lessons = db.relationship('Lesson', backref='board', lazy='dynamic')
+
+    def __init__(self, name, author_id, board_id):
+        self.name = name
+        self.author_id = author_id
+        self.board_id = board_id
+        self.time_created = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Chapter {}>'.format(self.name)
+
+
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    url = db.Column(db.String)
+    comment = db.Column(db.Text)
+    time_created = db.Column(db.DateTime)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'))
+
+    def __init__(self, title, url, comment, author_id, chapter_id):
+        self.title = title
+        self.url = url
+        self.comment = comment
+        self.author_id = author_id
+        self.chapter_id = chapter_id
+        self.time_created = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Lesson {}>'.format(self.url)
